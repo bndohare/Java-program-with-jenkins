cfd0769ee58ccce0c826fdd1e3a9861e2289f5b8
Bhaskar_git <bhaskar.dohare999@gmail.com>
2024-07-06 14:06:15 +0530

    change in code

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main{
      public static void main(String[] args) {
     System.out.println("This is java program tutorial using Jenkins ");
     System.out.println(new Date());
-    System.out.println("Hello");
+    System.out.println("Running project/build through git to jenkins");
 }
 }
