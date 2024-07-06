823da8e8b0a5050b924ce17d9950e0b2abfaa703
Bhaskar_git <bhaskar.dohare999@gmail.com>
2024-07-06 13:42:27 +0530

    Update Main.java
    initial sample code

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+
 public class Main{
      public static void main(String[] args) {
     System.out.println("This is java program tutorial using Jenkins ");
+    System.out.println(new Date());
+    System.out.println("Hello");
 }
 }
